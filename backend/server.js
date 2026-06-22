0cd2e4a29818cd7296d5b7df60de2a600ba79764
ManishTech19 <manish181210@gmail.com>
2026-06-22 19:52:26 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ const app = express();
 const port = 4000;
 
 // MIDDLEWARES
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
